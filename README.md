fa01dcc65c2cbc7ef64e2c053c3ea7d6eef57af8
Alfa <A.Nugraha@cgiar.org>
2020-03-06 13:23:14 +0700

    Second commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# lcd-scenario
-Low Carbon Development Scenario(s)
+# Low Carbon Development Scenario(s)
+The series of code for low development planning tools 
+
+# Usage
+1. To deploy the repository, please clone this site first
+2. To push your creative works, please create the folder by your initial name start with `underscore`. For example, `_AN`
+3. If you have any question, please write it in the issue tab
+4. Thank you :)
+
+# Data
+All raw data `(csv, xls, doc, rtf, pdf, ppt)`, please put in the **raw** folder. All compiled data `(RData, RDS)`, please put in the folder **data**.
---
+++ template.R
@@ -0,0 +1,53 @@
+###BEGIN: initiate all variables ####
+# username <- "alfanugraha"
+# password <- "1234"
+selectedProv = "JaBar"
+datapath <- paste0("data/", selectedProv, "/")
+# userFolder <- paste0(datapath, username)
+# if(!dir.exists(userFolder)) dir.create(userFolder, mode = 777)
+
+ioSector <- readRDS(paste0(datapath, "sector"))
+ioIntermediateDemand <- readRDS(paste0(datapath, "indem"))
+ioFinalDemand <- readRDS(paste0(datapath, "findem"))
+ioFinalDemandComponent <- readRDS(paste0(datapath, "findemcom"))
+ioAddedValue <- readRDS(paste0(datapath, "addval"))
+ioAddedValueComponent <- readRDS(paste0(datapath, "addvalcom"))
+ioLeontif <- readRDS(paste0(datapath, "I_A"))
+ioLeontiefInverse <- readRDS(paste0(datapath, "leontief"))
+ioPeriod <- readRDS(paste0(datapath, "periodIO"))
+
+satelliteLabour <- readRDS(paste0(datapath, "labour"))
+satelliteEnergy <- readRDS(paste0(datapath, "energy"))
+satelliteWaste <- readRDS(paste0(datapath, "waste"))
+emissionFactorEnergy <- readRDS(paste0(datapath, "ef_energy"))
+emissionFactorWaste <- readRDS(paste0(datapath, "ef_waste"))
+population <- readRDS(paste0(datapath, "currentPopulation"))
+populationProjection <- readRDS(paste0(datapath, "population"))
+baselineEmission <- readRDS(paste0(datapath, "otherEm"))
+
+# to be modified
+LU_tahun<-readRDS(paste0(datapath,"LU_tahun"))
+LDMProp_his<-readRDS(paste0(datapath,"LDMProp"))
+# row.names(LDMProp_his)<-sector[,1]
+# landtable <- readRDS(paste0(datapath, "landtable"))
+GDPAll <- readRDS(paste0(datapath, "GDPAll"))
+linkagesTable <- readRDS(paste0(datapath, "linkagesTable"))
+multiplierAll <- readRDS(paste0(datapath, "multiplierAll"))
+rtffile <- readRDS(paste0(datapath, "rtffile"))
+
+###END: initiate all variables ####
+
+
+matrixIoIntermediateDemand <- as.matrix(ioIntermediateDemand)
+matrixIoAddedValue <- as.matrix(ioAddedValue)
+nrowMatrixIoAddedValue <- nrow(matrixIoAddedValue)
+ioDimention <- ncol(ioIntermediateDemand)
+
+colSumsMatrixIoIntermediateDemand <- colSums(matrixIoIntermediateDemand)
+colSumsMatrixIoAddedValue <- colSums(matrixIoAddedValue)
+ioTotalOutput <- colSumsMatrixIoIntermediateDemand + colSumsMatrixIoAddedValue # ioTotalInput 
+ioTotalOutputInverse <- 1/ioTotalOutput
+ioTotalOutputInverse[is.infinite(ioTotalOutputInverse)] <- 0
+ioTotalOutputInverse <- diag(ioTotalOutputInverse)
+
+rowIncome <- 2
